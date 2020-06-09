0ce9be1e21953b416ffe3478e2f932d36a03ac0e
Spenser <spenser.davies@gmail.com>
2020-06-09 12:08:01 -0600

    more styling

--- clientsrc/src/views/Boards.vue
+++ clientsrc/src/views/Boards.vue
@@ -65,7 +65,7 @@
           
           <div class="col-12 my-5 text-center">
             <div class="row">
-            <form class="form-inline col-12" @submit.prevent="createTask">
+            <form class="form-inline col-12" @submit.prevent="createList">
               <div class="col-12 pb-3 border-bottom border-success">
               <div class="form-group">
                 <input type="text" name="title" id="title" class="form-control col-11 m-2" placeholder="New List Column..." aria-describedby="helpId" >
@@ -77,9 +77,47 @@
           </div>
         </div>
 
-        <div class="row">
-          <div class="col-3 mx-1 list-container rounded border border-danger bg-secondary">
-            text goes here
+        <div class="row text-left">
+          <div class="col-3 shadow mx-1 list-container rounded border border-danger bg-secondary">
+            
+            <div class="row mt-2">
+              <div class="col-7">
+                <p>List title</p>
+              </div>
+              <div class="col-5">
+                <span><i class="pointer text-warning fas fa-plus mx-2"></i><i class="pointer mx-2 fas fa-chevron-circle-right text-warning"></i><i class="pointer ml-2 fas fa-trash-alt text-warning"></i></span>
+              </div>
+              <div class="col-12">
+                <form class="form-inline col-12" @submit.prevent="createTask">
+                  <div class="form-group">
+                    <input type="text" name="title" id="title" class="form-control shadow" placeholder="new task..">
+                    <button type="submit" class="shadow mr-1 ml-2 btn btn-warning"><i class="text-success fas fa-plus"></i></button>
+                    <button type="button" class="shadow ml-1 btn btn-warning"><i class="fas fa-times text-success"></i></button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <hr>
+            <div class="row" id="this is for the tasks">
+              <div class="col-10 card shadow mx-auto my-2 pt-2">
+                <p>Task</p>
+                <small>added by: USER </small>
+                <hr>
+                <div class="row">
+                  <div class="col-12">
+                    <form class="form-inline" @submit.prevent="createComment">
+                      <div class="form-group col-12 mb-2">
+                        <input type="text" name="title" id="title" class="col-7 form-control shadow" placeholder="new comment..">
+                        <button type="submit" class="shadow rounded-circle mr-1 ml-2 btn btn-success"><i class="text-warning fas fa-plus"></i></button>
+                        <button type="button" class="shadow rounded-circle ml-1 btn btn-success"><i class="fas fa-times text-warning"></i></button>
+                     </div>
+                   </form>
+                 </div>
+                </div>
+                
+
+              </div>
+            </div>
           </div>
         </div>
 
@@ -175,6 +213,9 @@ export default {
 }
 .long-boy{
   height: 85vh;
+}
+.pointer{
+  cursor: pointer;
 }
 .text-left{
   text-align: left;
